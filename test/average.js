2134e4a9bd340fce0f00442d30b5193bfdc161a8
ken3527 <ken3527549@hotmail.com>
2017-09-08 13:42:51 +0800

    first commit

---
+++ test/average.js
@@ -0,0 +1,17 @@
+// test/should.js
+var should = require('should');
+var average = require('../lib/average');
+
+describe('#average', function(){
+    it('should return the average of array', function(done){
+        var avg = average([1, 2, 3, 4]);
+        avg.should.equal(3);      // 測試算出來的平均是不是 2.5
+        done();
+    })
+
+    it('should return NaN when array is empty', function(done){
+        var avg = average([]);
+        isNaN(avg).should.be.true;  // 測試有沒有回傳 NaN
+        done();
+    })
+})
